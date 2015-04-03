4a8d01191b515bbade8249f09adc205a7c7a0fbd
Robert Kawecki <robert+github@rkaw.pl>
2015-04-03 20:42:31 +0200

    Added an index.js so that the package may be require()d.

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports.AMQPConnector = require('./lib/AMQPConnector');
+module.exports.AMQPConsumer = require('./lib/AMQPConsumer');
+module.exports.AMQPListener = require('./lib/AMQPListener');
