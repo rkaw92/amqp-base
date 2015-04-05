05ea1ae63fa5e066b518acb1435168df3452ba5e
Robert Kawecki <robert+github@rkaw.pl>
2015-04-05 12:23:08 +0200

    Added the AMQPTieredListener to the index file.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.AMQPConnector = require('./lib/AMQPConnector');
 module.exports.AMQPConsumer = require('./lib/AMQPConsumer');
 module.exports.AMQPListener = require('./lib/AMQPListener');
+module.exports.AMQPTieredListener = require('./lib/AMQPTieredListener');
