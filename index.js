afe9aade07577d0ac66759b99c6bcc9a363330ca
Robert Kawecki <robert+github@rkaw.pl>
2015-04-06 23:53:13 +0200

    Added AMQPChannelManager to the exports in index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports.AMQPConnector = require('./lib/AMQPConnector');
 module.exports.AMQPConsumer = require('./lib/AMQPConsumer');
+module.exports.AMQPChannelManager = require('./lib/AMQPChannelManager');
 module.exports.AMQPListener = require('./lib/AMQPListener');
 module.exports.AMQPTieredListener = require('./lib/AMQPTieredListener');
